c0119069a3c651b353e8b01f2094a3292c53f14a
Dwayne Bailey <dwayne@translate.org.za>
2016-11-17 12:15:40 +0000

    Bump version to 0.28.1a1
    This will force downloading of our custom release until an official
    release.

--- allauth/__init__.py
+++ allauth/__init__.py
@@ -8,11 +8,11 @@
 
 """
 
-VERSION = (0, 28, 0, 'final', 0)
+VERSION = (0, 28, 1, 'a', 1)
 
 __title__ = 'django-allauth'
 __version_info__ = VERSION
-__version__ = '.'.join(map(str, VERSION[:3])) + ('-{}{}'.format(
+__version__ = '.'.join(map(str, VERSION[:3])) + ('{}{}'.format(
     VERSION[3], VERSION[4] or '') if VERSION[3] != 'final' else '')
 __author__ = 'Raymond Penners'
 __license__ = 'MIT'
